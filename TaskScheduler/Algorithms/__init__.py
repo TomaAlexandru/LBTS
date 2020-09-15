8b92754ff46606113284312d7691ac3611607178
Alexandru Toma <alexandru.toma@ionos.com>
2020-09-15 09:54:25 +0300

    testing commit

--- TaskScheduler/Algorithms/__init__.py
+++ TaskScheduler/Algorithms/__init__.py
@@ -3,4 +3,3 @@ from TaskScheduler.Algorithms.Random import Random
 from TaskScheduler.Algorithms.ShortestProcessingTime import ShortestProcessingTime
 from TaskScheduler.Algorithms.EarliestDueTime import EarliestDueTime
 from TaskScheduler.Algorithms.CriticalRatio import CriticalRatio
-from TaskScheduler.Algorithms.LatestDueTime import LatestDueTime
